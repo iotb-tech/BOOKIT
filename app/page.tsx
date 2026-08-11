198884944e54e818d94c54a1761bc48e42142ad2
Y-Ismail <ismailyunusk03@gmail.com>
2026-08-11 19:42:13 +0100

    add src

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import config from '../postcss.config.mjs';
-
-export default function Home(){
-  return (
-    <div className="text-3xl bg-primary-50">
-      This is the home page
-    </div>
-  )
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import config from "../../postcss.config.mjs";
+
+export default function Home() {
+  return <div className="text-3xl bg-primary-50">This is the home page</div>;
+}
