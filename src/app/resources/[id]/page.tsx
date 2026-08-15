e9deef70bb68c3b49a2e3095c664a247cde7a74a
RASHEEDA <my1onlinestores@gmail.com>
2026-08-15 10:54:19 +0100

    Complete resource details page

--- src/app/resources/[id]/page.tsx
+++ src/app/resources/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { createClient } from "@/lib/supabase/server";
+import { getResourceById } from "@/lib/resources";
+
 type ResourcePageProps = {
     params: Promise<{
         id: string
@@ -5,11 +8,28 @@ type ResourcePageProps = {
 };
 
 export default async function ResourcePage({ params} : ResourcePageProps) {
-    const { id } = await params;
+   const { id } = await params; 
+// Create a Supabase client.
+const supabase = await createClient();
+
+// Get the resource using the ID from the URL.
+const resource = await getResourceById(supabase, id);
+
+if (!resource) {
     return (
         <main>
-            <h1>Resource Details</h1>
-            <p>Resource ID:{id}</p>
+            <h1>Resource Not Found</h1>
+            <p>The requested resource could not be found.</p>
+        </main>
+    );
+}
+    return (
+        <main>
+            <h1>Resource.name</h1>
+            <p>{resource.description}</p>
+
+            {resource.type && <p>Type: {resource.type}</p>}
+            {resource.owner_name && <p>Owner: {resource.owner_name}</p>}
             </main>
 
     );
