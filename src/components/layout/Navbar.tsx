bb5d4c60e6b27397f9278f6096858521da6d4c66
habybheart1 <akintobiabibat@yahoo.com>
2026-08-15 16:11:29 +0100

    feat: logo added to the layout navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { User } from "@supabase/supabase-js";
+import { CalendarDays } from "lucide-react";
 
 export default function Navbar() {
   const [user, setUser] = useState<User | null>(null);
@@ -34,7 +35,14 @@ export default function Navbar() {
 
   return (
     <nav className="flex items-center justify-between p-4 border-b text-white bg-primary-600">
-      <Link href="/">BookIt</Link>
+      <Link href="/" className="flex items-center">
+        <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600 text-white">
+            <CalendarDays size={24} />
+          </div>
+          <span className="text-xl font-bold text-white">
+            BookIt
+          </span>
+      </Link>
 
       <div className="flex items-center gap-4">
         <Link href="/">Home</Link>
